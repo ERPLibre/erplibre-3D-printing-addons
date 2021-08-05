9fe8f205c5a6f4e81edbc35053eecbb32ab25c48
Normil Jose <josenormil@gmail.com>
2021-08-05 09:11:21 -0400

    Re-Adding web_slicing_server because of an unknown error

--- web_slicing_server/__manifest__.py
+++ web_slicing_server/__manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name': 'Server Management For SuperSlicer',
-    'category': 'Manufacturing',
-    'version': '12.0.1.0',
-    'author': 'Jose Normil',
-    'license': 'AGPL-3',
-    'application': True,
-    'data': [
-        'security/ir.model.access.csv',
-        'views/slicing_server.xml',
-        'views/menus.xml',
+    "name": "SuperSlicer Server Infos",
+    "category": "Manufacturing",
+    "summary": "SuperSlicer Server infos Management for the slicing operations",
+    "version": "12.0.1.0",
+    "author": "Jose Normil",
+    "license": "AGPL-3",
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/slicing_server.xml",
+        "views/menus.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- web_slicing_server/models/slicing_server.py
+++ web_slicing_server/models/slicing_server.py
@@ -4,18 +4,44 @@ from odoo import _, api, models, fields
 class SlicingServer(models.Model):
     _name = 'slicing.server'
     _description = 'SuperSlicer Server'
+    _sql_constraints = [('unique_ss_server', 'UNIQUE(name,address,port)',
+                         'The SuperSlicer Server infos should be unique!')]
 
-    ip = fields.Char(
-        string='IP Address',
+    address = fields.Char(
+        string="IP Address",
+        help="SuperSlicer Server Address [domain name ; ip]",
         required=True,
+        default="localhost",
+        index=True,
     )
 
     port = fields.Integer(
         string='Port',
+        help="Listening port of the SuperSlicer Server",
         required=True,
+        default=5000,
+        index=True,
     )
 
     name = fields.Char(
-        string='Name',
+        string="Name",
         required=True,
+        default="SuperSlicer Server",
+        index=True,
     )
+
+    default_server = fields.Boolean(
+        string="Default server?",
+        help="Use as the default SuperSlicer Server",
+        default=False,
+        index=True,
+    )
+
+    @api.onchange('default_server')
+    def _onchange_default_server(self):
+        if self.default_server:
+            result = self.search([('default_server', '=', True)], limit=1)
+            result.write({'default_server': False})
+        else:
+            result = self.search([('default_server', '=', False)], order='id', limit=1)
+            result.write({'default_server': True})
